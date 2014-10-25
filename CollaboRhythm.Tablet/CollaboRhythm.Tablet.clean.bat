ad32061318f5b3c38bca746ba00d8e47eb80989b
Bree Alyeska <breezy@alum.mit.edu>
2014-10-25 17:15:22 -0400

    Ant build configuration for Mac OS X
    updated build.xml and property files for mac os x file structure

--- CollaboRhythm.Tablet/CollaboRhythm.Tablet.clean.bat
+++ CollaboRhythm.Tablet/CollaboRhythm.Tablet.clean.bat
@@ -4,11 +4,11 @@
 :: ***************************************************************************************
 :: Note: before using this install script you should change the variables below
 
-set AndroidADBFolder=C:\Program Files (x86)\Android\android-sdk\platform-tools
+set AndroidADBFolder="/Applications/Android Studio.app/sdk/platform-tools"
 :: Android default path used to include the word "windows", so you might need this depending on the version you have
-::set AndroidADBFolder=C:\Program Files (x86)\Android\android-sdk-windows\platform-tools
+::set AndroidADBFolder=/Applications/Android Studio.app/sdk/platform-tools
 ::set SettingsFile=my_settings_debug.xml
-set SettingsFile=%USERPROFILE%\AppData\Roaming\CollaboRhythm.Tablet.debug\Local Store\settings.xml
+set SettingsFile="/Users/breezy/AppData/Roaming/CollaboRhythm.Tablet.debug/Local Store/settings.xml"
 set CollaboRhythmTabletApk=CollaboRhythm.Tablet.apk
 
 :: ***************************************************************************************
@@ -23,11 +23,11 @@ echo.
 echo Settings: %SettingsFile%
 echo.
 
-"%AndroidADBFolder%"\adb shell rm "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/settings.xml"
-"%AndroidADBFolder%"\adb shell rm "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/plugins/*"
-"%AndroidADBFolder%"\adb shell rmdir "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/plugins"
+"%AndroidADBFolder%"/adb shell rm "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/settings.xml"
+"%AndroidADBFolder%"/adb shell rm "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/plugins/*"
+"%AndroidADBFolder%"/adb shell rmdir "/data/local/air.CollaboRhythm.Tablet.debug/CollaboRhythm.Tablet.debug/Local Store/plugins"
 
-"%AndroidADBFolder%"\adb -d uninstall air.CollaboRhythm.Tablet.debug
+"%AndroidADBFolder%"/adb -d uninstall air.CollaboRhythm.Tablet.debug
 
 echo.
 echo Remove complete for %CollaboRhythmTabletApk%
--- CollaboRhythm.Tablet/CollaboRhythm.Tablet.remove.settings.bat
+++ CollaboRhythm.Tablet/CollaboRhythm.Tablet.remove.settings.bat
@@ -4,11 +4,11 @@
 :: ***************************************************************************************
 :: Note: before using this install script you should change the variables below
 
-set AndroidADBFolder=C:\Program Files (x86)\Android\android-sdk\platform-tools
+set AndroidADBFolder=/Applications/Android Studio.app/sdk/platform-tools
 :: Android default path used to include the word "windows", so you might need this depending on the version you have
-::set AndroidADBFolder=C:\Program Files (x86)\Android\android-sdk-windows\platform-tools
+::set AndroidADBFolder=/Applications/Android Studio.app/sdk/platform-tools
 ::set SettingsFile=my_settings_debug.xml
-set SettingsFile=%USERPROFILE%\AppData\Roaming\CollaboRhythm.Tablet.debug\Local Store\settings.xml
+set SettingsFile=/Users/breezy/AppData/Roaming/CollaboRhythm.Tablet.debug/Local Store/settings.xml
 
 :: ***************************************************************************************
 
